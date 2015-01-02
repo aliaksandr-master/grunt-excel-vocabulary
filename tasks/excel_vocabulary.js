225b2b6a14894f01fdd17440b9f9a4c02c680bf5
Aliaksandr Pasynkau <alxe.master@gmail.com>
2015-01-02 15:16:55 +0300

    add task logic

---
+++ tasks/excel_vocabulary.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var path = require('path');
+var parse = require('./../lib/parse');
+
+module.exports = function (grunt) {
+	grunt.registerMultiTask('excel_vocabulary', function () {
+		var options = this.options({
+            root: process.cwd(),
+            beautify: true
+		});
+
+		this.files.forEach(function (f) {
+			f.src.filter(function (filepath) {
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				}
+
+                return true;
+			}).forEach(function (srcFilePath) {
+				var resultJson = parse(path.resolve(options.root, srcFilePath));
+				var resultJsonString = JSON.stringify(resultJson, null, options.beautify ? 4 : null);
+
+				grunt.file.write(f.dest, resultJsonString);
+				grunt.log.writeln('File "' + f.dest + '" created.');
+			});
+		});
+	});
+};
