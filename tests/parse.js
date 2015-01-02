eb1b05b3138e3ac80b27d36db557cbcb89561267
Aliaksandr Pasynkau <alxe.master@gmail.com>
2015-01-02 17:13:49 +0300

    update parse tests

--- tests/parse.js
+++ tests/parse.js
@@ -4,16 +4,25 @@
 var parse = require('./lib/parse');
 
 module.exports['file with one empty sheet'] = function (test) {
-	var json = parse(__dirname + '/../examples/one-empty-sheet.xlsx');
+	test.doesNotThrow(function () {
+		var json = parse(__dirname + '/../examples/one-empty-sheet.xlsx');
+		test.deepEqual(json, require(__dirname + '/../examples/one-empty-sheet.json'));
+	});
 	test.done();
 };
 
 module.exports['file with one non empty sheet'] = function (test) {
-	var json = parse(__dirname + '/../examples/one-non-empty-sheet.xlsx');
+	test.doesNotThrow(function () {
+		var json = parse(__dirname + '/../examples/one-non-empty-sheet.xlsx');
+		test.deepEqual(json, require(__dirname + '/../examples/one-non-empty-sheet.json'));
+	});
 	test.done();
 };
 
 module.exports['file with two non empty sheet'] = function (test) {
-	var json = parse(__dirname + '/../examples/two-non-empty-sheets.xlsx');
+	test.doesNotThrow(function () {
+		var json = parse(__dirname + '/../examples/two-non-empty-sheets.xlsx');
+		test.deepEqual(json, require(__dirname + '/../examples/two-non-empty-sheets.json'));
+	});
 	test.done();
 };
